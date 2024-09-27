e1c5038223585d132e92bd63e4242d65e5337d33
lndsilva <lndsilva@hotmail.com>
2024-09-27 10:51:38 -0300

    cadastrar usuario

--- dbti112.sql
+++ dbti112.sql
@@ -4,13 +4,6 @@ create database dbti112;
 
 use dbti112;
 
-create table tbUsuarios(
-codUsu int not null auto_increment,
-nome varchar(30) not null,
-senha varchar(20) not null,
-primary key(codUsu)
-);
-
 create table tbFuncionarios(
 codFunc int not null auto_increment,
 nome varchar(50) not null,
@@ -25,15 +18,25 @@ cidade varchar(100),
 estado char(2),
 primary key(codfunc));
 
+create table tbUsuarios(
+codUsu int not null auto_increment,
+nome varchar(30) not null,
+senha varchar(20) not null,
+codFunc int not null,
+primary key(codUsu),
+foreign key(codfunc) references tbFuncionarios(codfunc));
+
+-- inserindo registros na tabela usuarios
+insert into tbUsuarios(nome,senha,codfunc)
+	values(nome,senha,codfunc);
+
 -- inserindo registros na tabela funcionarios
 
 insert into tbFuncionarios(nome,email,cpf,telCel,cep,endereco,numero,bairro,cidade,estado)values(nome,email,cpf,telCel,cep,endereco,numero,bairro,cidade,estado);
 
 -- alterando registros na tabela funcionarios
 
-update tbFuncionarios set nome=nome,email=email,cpf=cpf,telCel=telCel,
-	cep=cep,endereco=endereco,numero=numero,bairro=bairro,
-	cidade=cidade,estado=estado where codfunc = codfunc;
+update tbFuncionarios set nome=nome,email=email,cpf=cpf,telCel=telCel,cep=cep,endereco=endereco,numero=numero,bairro=bairro,cidade=cidade,estado=estado where codfunc = codfunc;
 
 -- excluindo registros na tabela funcionarios
 
@@ -51,7 +54,7 @@ select * from tbFuncionarios where nome like '%nome%';
 show tables;
 
 insert into tbUsuarios(nome,senha)
-	values('senac','senac');
+	values('admin',SHA('admin'));
 
 select * from tbUsuarios;
 
